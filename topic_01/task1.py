9609e91da92df62ed4dc0b4fca859142eb65e65b
Serhii <kravchenkoss1541@gmail.com>
2024-12-01 19:37:03 +0200

    add solution for topic 01

--- topic_01/task1.py
+++ topic_01/task1.py
@@ -1,2 +1,12 @@
 string = input("Уведіть будь-що щоб його обернути в зворотному порядку за допомогою слайсингу: ")
-print(string[::-1])
+
+print(f'\nПочатковий вигляд вашого вводу: {string}')
+print(f'У зворотному напрямі за допомогою слайсингу: {string[::-1]}')
+
+reversed_text = "".join(reversed(string))
+print(f'У зворотному напрямі за допомогою слайсингу: {reversed_text}')
+
+reversed_text = ""
+for char in string:
+    reversed_text = char + reversed_text
+print(f'У зворотному напрямі за допомогою циклу: {reversed_text}')
--- topic_01/task3.py
+++ topic_01/task3.py
@@ -1,9 +1,14 @@
-def find_discriminator():
+def find_discriminator(a, b, c):
     return b ** 2 - 4 * a * c
 
 
-a = int(input("Уведіть a: "))
-b = int(input("Уведіть b: "))
-c = int(input("Уведіть c: "))
+def input_call():
+    a = int(input("Уведіть a: "))
+    b = int(input("Уведіть b: "))
+    c = int(input("Уведіть c: "))
+    return a, b, c
 
-print(find_discriminator())
+
+coeffs = input_call()
+
+print(f'\nПри таких коефіцієнтах Дискримінант буде: {find_discriminator(coeffs[0], coeffs[1], coeffs[2])}')
